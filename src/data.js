810299f6199ad49be2f0722a40239e658a1142bc
Brandonarmand96 <66756596+Brandonarmand96@users.noreply.github.com>
2021-10-20 17:38:26 +0100

    updated links
    updated links

--- src/data.js
+++ src/data.js
@@ -1,34 +1,34 @@
 const products = [{
         id: 1,
-        img: "https://ibb.co/3kNSMtBhttps://ibb.co/RDNNVK4",
+        img: "https://i.ibb.co/71QQ6DN/expense-tracker.jpg",
         link: "https://expense-tracker-zeta-seven.vercel.app/"
     },
     {
         id: 2,
-        img: "https://ibb.co/hsWDK5V",
+        img: "https://i.ibb.co/NxjZLhK/github-1.png",
         link: "https://brandonarmand96.github.io/github-finder",
     },
     {
         id: 3,
-        img: "https://ibb.co/QJxQBwj",
+        img: "https://i.ibb.co/51J4DHx/hotel.jpg",
         link: "https://brandonarmand96.github.io/Hotel-website/",
     },
     {
         id: 4,
-        img: "https://ibb.co/QDC6nH6",
+        img: "https://i.ibb.co/6X14bt4/newspress.jpg",
         link: "https://brandonarmand96.github.io/Newspress/",
 
     },
     {
         id: 5,
-        img: "https://ibb.co/QKmffpK",
+        img: "https://i.ibb.co/YLj33PL/tindog.png",
         link: "https://brandonarmand96.github.io/TinDog-Website/",
 
 
     },
     {
         id: 6,
-        img: "https://ibb.co/3kNSMtB",
+        img: "https://i.ibb.co/KmqVLCF/screencapture-brandonarmand96-github-io-Drum-Kit-2021-10-20-11-41-40.png",
         link: "https://brandonarmand96.github.io/Drum-Kit/",
 
 
