9954527a8d2b99e1ad7ca175874a19a0936f1e7a
Brandonarmand96 <66756596+Brandonarmand96@users.noreply.github.com>
2021-10-20 17:16:55 +0100

    Initial commit

---
+++ src/data.js
@@ -0,0 +1,41 @@
+const products = [{
+        id: 1,
+        img: "https://ibb.co/3kNSMtBhttps://ibb.co/RDNNVK4",
+        link: "https://expense-tracker-zeta-seven.vercel.app/"
+    },
+    {
+        id: 2,
+        img: "https://ibb.co/hsWDK5V",
+        link: "https://brandonarmand96.github.io/github-finder",
+    },
+    {
+        id: 3,
+        img: "https://ibb.co/QJxQBwj",
+        link: "https://brandonarmand96.github.io/Hotel-website/",
+    },
+    {
+        id: 4,
+        img: "https://ibb.co/QDC6nH6",
+        link: "https://brandonarmand96.github.io/Newspress/",
+
+    },
+    {
+        id: 5,
+        img: "https://ibb.co/QKmffpK",
+        link: "https://brandonarmand96.github.io/TinDog-Website/",
+
+
+    },
+    {
+        id: 6,
+        img: "https://ibb.co/3kNSMtB",
+        link: "https://brandonarmand96.github.io/Drum-Kit/",
+
+
+    }
+
+
+]
+
+
+export default products;
